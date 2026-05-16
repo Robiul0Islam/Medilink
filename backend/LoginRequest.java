1e640b1df968fc2ae213f435da66307c5913445e
kim239055 <muntakimhossain64@gmail.com>
2026-05-17 00:32:05 +0600

    third commit

---
+++ backend/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.medilink.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
