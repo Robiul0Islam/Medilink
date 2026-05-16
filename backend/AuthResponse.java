042699ee6daed6026a3d005d2fac87d577c55fd1
kim239055 <muntakimhossain64@gmail.com>
2026-05-17 00:24:29 +0600

    added auth

---
+++ backend/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.medilink.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private Long userId;
+    private String email;
+    private String name;
+    private String role;
+    private String avatar;
+    private String city;
+}
